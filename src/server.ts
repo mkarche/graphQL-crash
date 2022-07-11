b8abcfe402aa172f162f2329e807c0f76961fc8f
mkarche <karchemohamed@gmail.com>
2022-07-11 23:01:17 +0100

    GraphQL Mutation Practice -- Create and Update

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import {
 	GraphQLSchema,
 	GraphQLString,
 } from "graphql";
+import { argsToArgsConfig } from "graphql/type/definition";
+import { resolve } from "path";
 
 // const expressGraphQL = require("express-graphql");
 // const buildSchema = require("graphql");
@@ -130,9 +132,104 @@ const RootQueryType = new GraphQLObjectType({
 	}),
 });
 
+const RootMutationType = new GraphQLObjectType({
+	name: "BookStoreMutation",
+	description: "BookStore Root Mutation",
+	fields: {
+		addBook: {
+			type: BookType,
+			description: "Add a single Book",
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				authorId: { type: GraphQLNonNull(GraphQLInt) },
+			},
+			resolve: (parent, args) => {
+				const book = {
+					id: books.length + 1,
+					name: args.name,
+					authorId: args.authorId,
+				};
+				books.push(book);
+				return book;
+			},
+		},
+		updateBook: {
+			type: BookWithAuthorsType,
+			description: "Update a single Book",
+			args: {
+				id: { type: GraphQLNonNull(GraphQLInt) },
+				newName: { type: GraphQLString },
+				newAuthorId: { type: GraphQLInt },
+			},
+			resolve: (parent, args) => {
+				const bookIndex = books.findIndex((book) => book.id === args.id);
+				console.log("Book to update", bookIndex);
+
+				if (bookIndex < 0 || bookIndex === null || bookIndex === undefined) {
+					return;
+				}
+
+				if (args.newName) {
+					books[bookIndex].name = args.newName;
+				}
+
+				if (args.newAuthorId) {
+					books[bookIndex].authorId = args.newAuthorId;
+				}
+
+				return books[bookIndex];
+			},
+		},
+		addAuthor: {
+			type: AuthorType,
+			description: "Add a single Author",
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+			},
+			resolve: (parent, args) => {
+				const author = {
+					id: authors.length + 1,
+					name: args.name,
+				};
+				authors.push(author);
+				return author;
+			},
+		},
+		updateAuthor: {
+			type: AuthorWithBooksType,
+			description: "Update a single Author",
+			args: {
+				id: { type: GraphQLNonNull(GraphQLInt) },
+				newName: { type: GraphQLString },
+			},
+			resolve: (parent, args) => {
+				const authorIndex = authors.findIndex(
+					(author) => author.id === args.id
+				);
+				console.log("Book to update", authorIndex);
+
+				if (
+					authorIndex < 0 ||
+					authorIndex === null ||
+					authorIndex === undefined
+				) {
+					return;
+				}
+
+				if (args.newName) {
+					authors[authorIndex].name = args.newName;
+				}
+
+				return authors[authorIndex];
+			},
+		},
+	},
+});
+
 const schema = new GraphQLSchema({
 	description: "BookStore Schema",
 	query: RootQueryType,
+	mutation: RootMutationType,
 });
 
 app.use(
@@ -140,7 +237,6 @@ app.use(
 	graphqlHTTP({
 		graphiql: true,
 		schema: schema,
-		//rootValue: root,
 	})
 );
 
